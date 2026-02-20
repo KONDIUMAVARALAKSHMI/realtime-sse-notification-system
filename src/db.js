8e6617fc0aff1ca2f9aa90799458bfa3e73331dd
kondi varalakshmi <umakondi167@gmail.com>
2026-02-20 20:11:39 +0530

    fix: add connection test on startup to ensure database is reachable

--- src/db.js
+++ src/db.js
@@ -9,7 +9,17 @@ const pool = new Pool({
     connectionTimeoutMillis: 2000, // how long to wait before timing out when connecting a new client
 });
 
-
+pool.on('connect', client => {
+    client.query('SELECT NOW()', (err, result) => {
+        // In a 'connect' event, the client is already connected.
+        // We don't need to call client.release() here as it's not a client acquired via pool.connect().
+        // The client will be managed by the pool.
+        if (err) {
+            return console.error('Error executing test query on new client connection', err.stack);
+        }
+        console.log('Database client connected and tested successfully');
+    });
+});
 
 pool.on('error', (err) => {
     console.error('Unexpected error on idle client', err);
